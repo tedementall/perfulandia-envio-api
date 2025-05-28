8e8fb4c48919761816ba082a4fee048b89ac687c
Andru Alarcon <idazhdz@gmail.com>
2025-05-27 22:53:07 -0400

    Commit inicial del proyecto

---
+++ src/main/java/com/perfulandia/perfulandia_envio_api/PerfulandiaEnvioApiApplication.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_envio_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerfulandiaEnvioApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerfulandiaEnvioApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perfulandia/perfulandia_envio_api/PerfulandiaEnvioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.perfulandia_envio_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerfulandiaEnvioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
